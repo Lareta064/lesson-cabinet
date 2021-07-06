ed038fdb6d072d8f47fc45ed058963622b1e8cdd
Lareta <lareta064@gmail.com>
2021-07-06 13:49:36 +0300

    full-footer

--- src/script/lesson.js
+++ src/script/lesson.js
@@ -2,11 +2,13 @@ $(document).ready(function () {
 	/*закрыть модальное окно по клику на крестик*/
 	let menuButtons = document.querySelectorAll('button[data-target]');
 	let modalWrappers = document.querySelectorAll('.modal-wrap');
+	const overlayBlock = document.querySelector('#overlay');
 	for(let modal of modalWrappers){
 		modal.addEventListener('click', function(e){
 			e.stopPropagation();
 			if(e.target.classList.contains('close-modal') ){
 				this.classList.remove('show');
+				overlayBlock.classList.remove('show');
 			}
 		})
 	}
@@ -16,12 +18,19 @@ $(document).ready(function () {
 	for(let item of menuButtons){
 		item.addEventListener('click', function(){
 			let ThisData = item.getAttribute('data-target');
+			/*Показать затемнение*/
+			if(item.classList.contains('overlay-bg')){
+				overlayBlock.classList.add('show');
+				console.log('456');
+			}
+			/*Показать модальное окно*/
 			for(let modal of modalWrappers){
 				let modalData = modal.getAttribute('data-role');
 				if(modalData == ThisData){
 					modal.classList.add('show');
 				}
 			}
+			
 		})
 	}
 			
